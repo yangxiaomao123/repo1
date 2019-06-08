130bfcaa36d8d01cc9d199af45519459aecd1a5d
root <root@123.com>
2019-06-08 19:40:08 +0800

    添加test工程到git

---
+++ code-git/code-git01/src/main/java/cn/itcast/GitTest.java
@@ -0,0 +1,18 @@
+package cn.itcast;
+
+/**
+ * @author yangx
+ * @date 2019/6/8
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git,git");
+        System.out.println("git,git");
+        System.out.println("git,git");
+        System.out.println("git,git");
+        System.out.println("git,git");
+        System.out.println("git,git");
+    }
+
+}
